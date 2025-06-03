2399e8c6712938f8d1fcb3c81c70d877d60123ef
RanaUniverse <RanaUniverse321@gmail.com>
2025-06-03 20:13:34 +0530

    screenshot script got updated with some notification goodly

--- files_and_folders/scripts_for_shortcuts/fullscreen_screenshot.sh
+++ files_and_folders/scripts_for_shortcuts/fullscreen_screenshot.sh
@@ -1,3 +1,14 @@
-# This will capture full screen and save
 
-xfce4-screenshooter -c -f -s ~/Pictures/screenshots/Screenshot_$(date +"%Y-%m-%d_%H-%M-%S.%3N").png
+# Generate current timestamp once to use in filename
+
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S-%3N")
+image_filename="full_screenshot_$timestamp.png"
+
+save_path=~/Pictures/screenshots/$image_filename
+
+# Take fullscreen screenshot and save in folder and in clipboard
+xfce4-screenshooter -c -f -s "$save_path"
+
+
+# Send notification with clipboard and filename info
+notify-send -t 5000 "✅ Screenshot Saved Successfully" "\n🖼️ $image_filename\n📋 Image also copied to Clipboard.\n🍌 By- Rana Universe"
